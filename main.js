1579b608b67e5d260e7a62ce82353d4ec3746662
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-16 10:37:21 +0300

    add lab-api

---
+++ main.js
@@ -0,0 +1,36 @@
+let url = "https://fakestoreapi.com/products";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+
+    let div = document.createElement("div");
+        div.classList="div"
+
+    div.innerHTML = '';
+
+    data.forEach((e) => {
+      let r=document.createElement("div")
+      let c=r.classList="div2"
+      
+      let img = document.createElement("img");
+      img.classList="img"
+      img.src = e.image;
+  
+
+      let pTitle = document.createElement("p");
+      pTitle.textContent = e.title;
+
+      let pPrice = document.createElement("p");
+      pPrice.textContent = e.price;
+
+      r.appendChild(img);
+      r.appendChild(pTitle);
+      r.appendChild(pPrice); 
+      div.appendChild(r);
+
+      document.body.appendChild(div);
+    });
+
+  })
